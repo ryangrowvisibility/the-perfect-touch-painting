73b9c5468c5acf692e5dc3ccc4f1788e8678459d
Ryan Irwin <rcirwin11@gmail.com>
2026-05-07 21:02:45 -0400

    fix: add unoptimized:true to fix Google CDN images on Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
+      { protocol: "https", hostname: "**.googleusercontent.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "*.googleusercontent.com" },
+      { protocol: "https", hostname: "**.yelpcdn.com" },
       { protocol: "https", hostname: "maps.googleapis.com" },
     ],
   },
